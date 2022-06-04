547c1f5eb26ef23b1c67c02a97e98e6b23eddd7d
bibhudh <nepalbib@gmail.com>
2022-06-04 21:22:09 +0545

    update

---
+++ ConditionalStatements/if-else.py
@@ -0,0 +1,14 @@
+# sum of two positive numbers
+print('Numbers must be positive')
+a = int(input('Enter a number: '))
+b = int(input('Enter another number: '))
+if a<0 or b<0:
+    print('One of the numbers is not positive')
+else:
+    sum = a+b
+    print('The sum is', sum)
+
+
+
+
+
